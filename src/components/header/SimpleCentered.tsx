0a967975d79b896e405c952c0b08999c6e536c38
Adam Joseph Arling <aarling@gmail.com>
2023-09-10 13:42:48 -0500

    Add transition effect to mobile nav overlay

--- src/components/header/SimpleCentered.tsx
+++ src/components/header/SimpleCentered.tsx
@@ -1,7 +1,6 @@
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import React, { useState } from "react";
-
-import { Dialog } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
+import React, { Fragment, useState } from "react";
 
 interface Props {
   altReferenceTitle: string;
@@ -80,52 +79,67 @@ export default function HeaderSimpleCentered({
       </nav>
 
       {/* Mobile menu */}
-      <Dialog
-        as="div"
-        className="lg:hidden"
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-      >
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 left-0 z-10 w-full px-6 py-6 overflow-y-auto bg-gray-900">
-          <div className="flex items-center justify-between">
-            <div className="flex flex-1">
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-100"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon className="w-6 h-6" aria-hidden="true" />
-              </button>
+      <Transition show={mobileMenuOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="lg:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div>
+              <div className="fixed inset-0 z-10 " />
+              {/* Mobile menu overlay, show/hide based on mobile menu state. Black by default */}
+              <Dialog.Panel className="fixed inset-y-0 left-0 z-10 w-full px-6 py-6 overflow-y-auto bg-black">
+                <div className="flex items-center justify-between">
+                  <div className="flex flex-1">
+                    <button
+                      type="button"
+                      className="-m-2.5 rounded-md p-2.5 text-gray-100"
+                      onClick={() => setMobileMenuOpen(false)}
+                    >
+                      <span className="sr-only">Close menu</span>
+                      <XMarkIcon className="w-6 h-6" aria-hidden="true" />
+                    </button>
+                  </div>
+                  <a href="#" className="-m-1.5 p-1.5 text-gray-50">
+                    <span className="sr-only">{altReferenceTitle}</span>
+                    {logo ? (
+                      <img
+                        className="w-auto h-20"
+                        src={logo}
+                        alt={altReferenceTitle}
+                      />
+                    ) : (
+                      <span>{altReferenceTitle}</span>
+                    )}
+                  </a>
+                  <div className="flex justify-end flex-1"></div>
+                </div>
+                <div className="mt-6 space-y-2">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-50 hover:bg-gray-900"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </Dialog.Panel>
             </div>
-            <a href="#" className="-m-1.5 p-1.5 text-gray-50">
-              <span className="sr-only">Your Company</span>
-              {logo ? (
-                <img
-                  className="w-auto h-20"
-                  src={logo}
-                  alt={altReferenceTitle}
-                />
-              ) : (
-                <span>{altReferenceTitle}</span>
-              )}
-            </a>
-            <div className="flex justify-end flex-1"></div>
-          </div>
-          <div className="mt-6 space-y-2">
-            {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-50 hover:bg-gray-800"
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
-        </Dialog.Panel>
-      </Dialog>
+          </Transition.Child>
+        </Dialog>
+      </Transition>
     </header>
   );
 }
